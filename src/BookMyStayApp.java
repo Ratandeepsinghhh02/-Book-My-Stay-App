6fd380b8b16eeacd3937c67e309a696f5a4c8d5a
Ratandeepsinghhh02 <rs0782@srmist.edu.in>
2026-04-01 09:26:28 +0530

    Added room abstraction with Single, Double, and Suite types along with static availability display (v2.1)

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,3 +1,48 @@
+abstract class Room {
+    String type;
+    int beds;
+    double price;
+
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    abstract void display();
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+
+    void display() {
+        System.out.println(type + " | Beds: " + beds + " | Price: " + price);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+
+    void display() {
+        System.out.println(type + " | Beds: " + beds + " | Price: " + price);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+
+    void display() {
+        System.out.println(type + " | Beds: " + beds + " | Price: " + price);
+    }
+}
+
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
@@ -7,8 +52,23 @@ public class BookMyStayApp {
         System.out.println(" Welcome to Book My Stay Application ");
         System.out.println(" Hotel Booking System v1.0 ");
         System.out.println("======================================");
+        Room single = new SingleRoom();
+        Room dbl = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        System.out.println("Application started successfully.");
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        single.display();
+        System.out.println("Available: " + singleAvailable);
+
+        dbl.display();
+        System.out.println("Available: " + doubleAvailable);
+
+        suite.display();
+        System.out.println("Available: " + suiteAvailable);
+
         System.out.println("Application terminated.");
     }
 }
