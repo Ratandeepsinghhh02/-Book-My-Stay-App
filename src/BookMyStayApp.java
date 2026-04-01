b9dd4180a682044e07a25d583f055bbfeb083ce9
Ratandeepsinghhh02 <rs0782@srmist.edu.in>
2026-04-01 09:13:03 +0530

    Base application

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,6 @@
+public class BookMyStayApp{
+    public static void main(String[] args){
+        System.out.println("Welcome to BookMyStayApp ");
+        System.out.println("version : 1.0");
+    }
+}
